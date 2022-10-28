b21e352bf9974189a733a3c280dbc7af085f187d
02dixitaditya <37414494+02dixitaditya@users.noreply.github.com>
2022-10-29 01:54:43 +0530

    Update Profile.js

--- src/Profile.js
+++ src/Profile.js
@@ -1,14 +1,22 @@
 import axios from "axios";
 import FileSaver from "file-saver";
 import React, { useContext } from "react";
+import { useState } from "react";
 import { UserContext } from "./App";
 
 export default function Profile({ setPage }) {
   const [user] = useContext(UserContext);
+  const [state, setState] = useState({
+    type: user.type,
+    username: user.username,
+    password: user.password,
+    kind: user.kind,
+  });
+
   if (!user || !user.token) {
     setPage(1);
     return <></>;
-  } else {
+  } else {  
     const handleDownload = async (file) => {
       console.log(file);
       const res = await axios.get(`/fetch_files/${file.id}`, {
@@ -21,6 +29,37 @@ export default function Profile({ setPage }) {
         },
       });
       FileSaver.saveAs(res.data, file.filename);
+    };
+
+    const onChange = (e) => {
+      if (e.target.name == "file")
+        setState({ ...state, [e.target.name]: e.target.files });
+    };
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      try {
+        const formData = new FormData();
+        if (state.file)
+          for (let i = 0; i < state.file.length; i++) {
+            formData.append("file", state.file[i]);
+          }
+  
+        //-----------update_user instead of register---------------------
+        const res = await axios.post("/register", {
+          ...state,
+        });
+  
+        if (state.file)
+          await axios.post("/upload_files", formData, {
+            headers: {
+              "Content-Type": "multipart/form-data",
+              Authorization: res.data.token,
+            },
+          });
+      } catch (e) {
+        console.log(e);
+      }
     };
 
     return (
@@ -54,6 +93,30 @@ export default function Profile({ setPage }) {
             </>
           );
         })}
+
+        <div>
+
+        <form
+          onSubmit={(e) => {
+            handleSubmit(e);
+          }}
+          method="POST"
+          action="upload_files"
+          encType="multipart/form-data"
+          id="profileForm"
+        >
+        <h4>Upload files:</h4>
+        <input
+          type="file"
+          id="file"
+          name="file"
+          multiple
+          onChange={onChange}
+        />
+        <input type="submit" value="Upload" />
+      </form>
+
+        </div>
       </>
     );
   }
