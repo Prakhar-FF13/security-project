46c34fec456c49cbc0552424e251bbad3a26343b
02dixitaditya <37414494+02dixitaditya@users.noreply.github.com>
2022-11-24 12:22:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express"),
   http = require("http"),
   cors = require("cors"),
   https = require("https"),
+  crypto = require("crypto"),
   app = express(),
   options = {
     key: fs.readFileSync("./cert/key.pem", { encoding: "utf-8" }),
@@ -14,6 +15,45 @@ const express = require("express"),
   utils = require("./utils");
 
 require("./passport")(passport);
+
+/**************************************
+ * Pub/priv key for user to generate digital signature.
+ **************************************/
+
+function genKeyPairU() {
+  // Generates an object where the keys are stored in properties `privateKey` and `publicKey`
+  const keyPair = crypto.generateKeyPairSync("rsa", {
+    modulusLength: 4096, // bits - standard for RSA keys
+    publicKeyEncoding: {
+      type: "pkcs1", // "Public Key Cryptography Standards 1"
+      format: "pem", // Most common formatting choice
+    },
+    privateKeyEncoding: {
+      type: "pkcs1", // "Public Key Cryptography Standards 1"
+      format: "pem", // Most common formatting choice
+    },
+  });
+
+  return { publicKey: keyPair.publicKey, privateKey: keyPair.privateKey };
+}
+
+/**************************************
+ * Symmetric key algorithm below.
+ **************************************/
+
+const algorithm = "aes-256-cbc";
+const IV_LENGTH = 16;
+
+const symmetricEncrypt = (text) => {
+  const key = fs.readFileSync("./cryptography/id_sym_key.pem", {
+    encoding: "utf-8",
+  });
+  const iv = crypto.randomBytes(IV_LENGTH);
+  const cipher = crypto.createCipheriv(algorithm, Buffer.from(key, "hex"), iv);
+  let encrypted = cipher.update(text);
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+  return `${iv.toString("hex")}:${encrypted.toString("hex")}`;
+};
 
 // check if https connection is made or not.
 function isSecure(req) {
@@ -56,7 +96,68 @@ app.post(
   "/upload_files",
   passport.authenticate("jwt", { session: false }),
   async (req, res) => {
+    req.fileOrigin = "upload";
     return await mongoDB.upload(req, res);
+  }
+);
+
+app.post(
+  "/share",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    await mongoDB.users.updateOne(
+      {
+        _id: req.user._id,
+      },
+      {
+        $push: {
+          sent: {
+            origin: "sharedWith",
+            id: req.body.payload.id,
+            sendTo: req.body.sendTo,
+          },
+        },
+      }
+    );
+
+    await mongoDB.users.updateOne(
+      {
+        email: req.body.sendTo,
+      },
+      {
+        $push: {
+          received: {
+            origin: "received",
+            ...req.body,
+            receivedFrom: req.user.email,
+            receivedFromId: req.user._id,
+          },
+        },
+      }
+    );
+
+    return res.status(201).send({
+      message: "Shared the file",
+    });
+  }
+);
+
+app.get(
+  "/fetch_shared_files",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const data = await mongoDB.users.findOne({ _id: req.user._id });
+    return res.status(200).send(data.sent);
+  }
+);
+
+app.get(
+  "/fetch_received_files",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const data = await mongoDB.users.findOne({ _id: req.user._id });
+
+    return res.status(200).send(data.received);
   }
 );
 
@@ -77,21 +178,35 @@ app.get(
   }
 );
 
+app.post(
+  "/verify_file",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    return await mongoDB.verifyFile(req, res);
+  }
+);
+
 // register user.
 app.post("/register", (req, res) => {
   const saltHash = utils.genPassword(req.body.password),
     salt = saltHash.salt,
-    hash = saltHash.hash;
+    hash = saltHash.hash,
+    keyPairU = genKeyPairU(),
+    pubKeyU = keyPairU.publicKey,
+    privKeyUEnc = symmetricEncrypt(keyPairU.privateKey);
 
   mongoDB.users.insertOne(
     {
-      username: req.body.username,
+      email: req.body.email,
       type: req.body.type,
       hash,
       salt,
       kind: req.body.kind,
+      wallet: req.body.wallet,
       verified: false,
       files: [],
+      pubKey: pubKeyU,
+      privKeyEnc: privKeyUEnc,
     },
     (err, result) => {
       if (err) {
@@ -109,7 +224,7 @@ app.post("/register", (req, res) => {
 
 // login user.
 app.post("/login", (req, res) => {
-  mongoDB.users.findOne({ username: req.body.username }, function (err, user) {
+  mongoDB.users.findOne({ email: req.body.email }, function (err, user) {
     if (err) {
       return res.status(500).json({
         success: false,
@@ -130,6 +245,11 @@ app.post("/login", (req, res) => {
       user.salt
     );
 
+    delete user.hash;
+    delete user.salt;
+    delete user.pubKey;
+    delete user.privKeyEnc;
+
     if (isValid) {
       const token = utils.issueJWT(user);
       return res.status(200).json({
@@ -146,6 +266,51 @@ app.post("/login", (req, res) => {
     });
   });
 });
+
+
+app.post(
+  "/walletpay",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const data = await mongoDB.users.updateOne(
+      {
+        _id: req.user._id,
+      },
+      {
+        $set: {"wallet": req.body.wallet}
+      }
+    );
+    const fetchedData = await mongoDB.users.findOne(
+      {
+        _id: req.user._id,
+      },
+    );
+    return res.status(200).send(fetchedData.wallet);
+  }
+);
+
+
+app.post(
+  "/walletadd",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const data = await mongoDB.users.updateOne(
+      {
+        _id: req.user._id,
+      },
+      {
+        $set: {"wallet": req.body.wallet}
+      }
+    );
+    const fetchedData = await mongoDB.users.findOne(
+      {
+        _id: req.user._id,
+      },
+    );
+    return res.status(200).send(fetchedData.wallet);
+  
+  }
+);
 
 const httpServer = http.createServer(app);
 const httpsServer = https.createServer(options, app);
